8dd9e461207eff3d38c6baad9751ad7cbf2c6ee0
temporovtemp <temporovtemp@gmail.com>
2026-03-28 12:28:45 +0300

    Init commit

---
+++ src/main/java/com/example/tempsms/TempsmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tempsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempsmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tempsms/TempsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
